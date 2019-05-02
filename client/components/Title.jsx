9d373cdb2abe2f068310e288304d2d7ee1446aa1
sam-by-name <sam-sanders@hotmail.co.uk>
2019-05-02 22:27:51 +1200

    some styles

--- client/components/Title.jsx
+++ client/components/Title.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const Title = () => {
   return (
-    <div>
+    <div class="title">
       <h3>The magical</h3>
       <h2>The mythical</h2>
       <h1>Dandy Eleanor Sanders</h1>
--- server/public/bundle.js
+++ server/public/bundle.js
@@ -227,7 +227,9 @@ __webpack_require__.r(__webpack_exports__);
 
 
 var Title = function Title() {
-  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", null, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "The magical"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", null, "The mythical"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Dandy Eleanor Sanders"));
+  return react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("div", {
+    "class": "title"
+  }, react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h3", null, "The magical"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h2", null, "The mythical"), react__WEBPACK_IMPORTED_MODULE_0___default.a.createElement("h1", null, "Dandy Eleanor Sanders"));
 };
 
 /* harmony default export */ __webpack_exports__["default"] = (Title);
@@ -24215,7 +24217,7 @@ if (false) {} else {
 /*!***************************************************************!*\
   !*** ./node_modules/react-router-dom/esm/react-router-dom.js ***!
   \***************************************************************/
-/*! exports provided: MemoryRouter, Prompt, Redirect, Route, Router, StaticRouter, Switch, generatePath, matchPath, withRouter, __RouterContext, BrowserRouter, HashRouter, Link, NavLink */
+/*! exports provided: BrowserRouter, HashRouter, Link, NavLink, MemoryRouter, Prompt, Redirect, Route, Router, StaticRouter, Switch, generatePath, matchPath, withRouter, __RouterContext */
 /***/ (function(module, __webpack_exports__, __webpack_require__) {
 
 "use strict";
