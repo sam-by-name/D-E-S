e2c1f50cd89dceac708a0afe3c214fc10012f9aa
sam-by-name <sam-sanders@hotmail.co.uk>
2019-05-02 20:15:55 +1200

    hashrouter setup and Menu component created

--- client/components/App.jsx
+++ client/components/App.jsx
@@ -1,9 +1,22 @@
-import React from 'react'
+import React, {Component} from 'react'
+import {HashRouter as Router, Route, Link} from 'react-router-dom'
 
-const App = () => {
-  return (
-    <h1>D-E-S development has begun!</h1>
-  )
+import Menu from './Menu'
+
+class App extends Component {
+  constructor (props) {
+    super(props)
+    this.state = {
+      loaded: false
+    }
+  }
+  render() {
+    return (
+      <Router>
+        <Route path='/menu' component={Menu} />
+      </Router>
+    )
+  }
 }
 
 export default App
---
+++ client/components/Menu.jsx
@@ -0,0 +1,11 @@
+import React, {Component} from 'react'
+
+class Menu extends Component {
+  render() {
+    return (
+      <h1>D-E-S development has begun!</h1>
+    )
+  }
+}
+
+export default Menu
